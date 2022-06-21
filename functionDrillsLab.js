7a201185ec3758f5fa250abb6a8b3887ad33d52a
Quintin Vomocil <quintinvomocil@gmail.com>
2022-06-21 17:25:57 -0600

    Finished through Q 20

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -336,36 +336,81 @@ let myMovie = bestMovie("Lord of the Rings")
 ////////////////// PROBLEM 15 ////////////////////
 let bigOrSmallArray = [1,101, 102, 2, 103, 4, 5, 6, 107]
 /* 
-  Create a function called 'bigOrSmall' that takes in one parameter, 'arr',  which will be an array of numbers. 
+  Create a function called 'bigOrSmall' that takes in one parameter, 'arr',  
+  which will be an array of numbers. 
   Inside of the bigOrSmall function, create a new array called 'answers'. 
-  Then, loop over the passed in arr parameter, and check to see if the number in the array is GREATER than 100. 
+  Then, loop over the passed in arr parameter, and check to see if the number 
+  in the array is GREATER than 100. 
   If it is, push 'big' as a string to the answers array. 
-  If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. 
-  Return the answers array inside of the function to a variable called `arrayEvaluator`.
+  If the number is LESS than or EQUAL to 100, push 'small' as a string to the
+  answers array. 
+  Return the answers array inside of the function to a variable called 
+  `arrayEvaluator`.
 */
 
 //CODE HERE
 
+function bigOrSmall(arr){
+  let answers = [];
 
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i] > 100){
+      answers.push("Big")
+    } else {(arr[i] <= 100)
+        answers.push("Small")
+      } 
+    }
+    return answers
+  }
+
+  let arrayElevator = bigOrSmall(bigOrSmallArray)
+
+  //console.log(arrayElevator)
 ////////////////// PROBLEM 16 ////////////////////
+
+/*
+  Write a function that is called theEliminator, which takes in two arguments, 
+  contestants (which will each be an array of strings), and loser 
+  (which will be a string).
+  The function should loop over the array of contestant names. 
+  If the loser string appears in the array, splice it out. 
+  Return the new contestants array.
+*/
+
+//CODE HERE
+
 let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
 let loser = 'Glimmer'
-/*
-  Write a function that is called theEliminator, which takes in two arguments, contestants (which will each be an array of strings), and loser (which will be a string).
-  The function should loop over the array of contestant names. If the loser string appears in the array, splice it out. Return the new contestants array.
-*/
 
-//CODE HERE
+function theEliminator(contestants, loser){
 
+    for (let i=0; i < contestants.length; i++){
+      if (contestants[i] === loser){
+          contestants.splice(i, 1)
+      }
+      return contestants
+    }
+}
+
+let newContestants = theEliminator(contestants, loser)
+
+console.log(newContestants)
 
 ////////////////// PROBLEM 17 ////////////////////
 let sampleString = "Hi, my name is Kylo."
 /*
-  Write a function that takes in one argument, a string. The function should then console.log that string, in entirely uppercase characters.
+  Write a function that takes in one argument, a string. 
+  The function should then console.log that string, in entirely uppercase characters.
   Invoke the function, passing in the sampleString (above).
 */
 
 //CODE HERE
+
+function newSampleString(str) {
+    console.log(str.toUpperCase())
+}
+
+  newSampleString(sampleString)
 
 
 ////////////////// PROBLEM 18 ////////////////////
@@ -379,18 +424,61 @@ let sampleString = "Hi, my name is Kylo."
   return 'must provide a valid email address'
 */
 
+function emailCheck(email) {
+    email = String(email).trim()
+
+    if (email.includes('@')){
+      return 'email verified'
+    }  else {
+      return 'must provide a valid email address'
+    }
+}
+
 ////////////////// PROBLEM 19 ////////////////////
 /*
-  Write a function, naming it whatever you believe to be appropriate, that buys as many chocolate frogs as possible with a certain amount of gold. Each chocolate frog costs 3 gold. Your function should take in a single parameter, which is the amount of gold you are willing to spend. Your function should return a total amount of chocolate frogs you were able to purchase.
-  Create a variable called `totalFrogs` and set it equal to your function invoked, passing in the amount of gold you are willing to spend.
+  Write a function, naming it whatever you believe to be appropriate, 
+  that buys as many chocolate frogs as possible with a certain amount of gold. 
+  Each chocolate frog costs 3 gold. 
+  Your function should take in a single parameter, which is the amount of gold 
+  you are willing to spend. 
+  Your function should return a total amount of chocolate frogs you were 
+  able to purchase.
+  Create a variable called `totalFrogs` and 
+  set it equal to your function invoked, 
+  passing in the amount of gold you are willing to spend.
 */
 
 //CODE HERE
 
+let willingToSpend = 30
+
+function howManyFrogsIGot(amounttospend){
+    if(willingToSpend > 0){ 
+      willingToSpend -= 3
+  } return willingToSpend
+}
+
+
+// This just took 3 from the total and subtracted it one time.
+// This is what the solutions came up with. I think I am making the problems to
+// complicated. Just take a minute to find a simple solution.
+
+function frogBuyer(gold){
+  return gold / 3
+}
+
+let totalFrogs = frogBuyer(50)
 
 ////////////////// PROBLEM 20 ////////////////////
 /*
-  You might have noticed a slight bug in the previous problem. If you were to pass in 4 gold, the function would return to you 1.3333... However, you can't really go to a store and by 1.333 products. You would just be able to purchase 1 product. Re-write the function you used in the previous problem (give it the same name, just add a 2 to the end of it) that fixes this bug. Invoke the function and store the returned value to a variable called `totalFrogs2`.
+  You might have noticed a slight bug in the previous problem. 
+  If you were to pass in 4 gold, the function would return to you 1.3333... 
+  However, you can't really go to a store and by 1.333 products. 
+  You would just be able to purchase 1 product. 
+  Re-write the function you used in the previous problem 
+  (give it the same name, just add a 2 to the end of it) that fixes this bug. 
+  Invoke the function and store the returned value to a 
+  variable called `totalFrogs2`.
 */
 
 //CODE HERE
@@ -399,10 +487,21 @@ let sampleString = "Hi, my name is Kylo."
 ////////////////// PROBLEM 21 ////////////////////
 let sampleArray = [0,1,2,3,4,7,5,6,8,9]
 /*
-  Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
+  Write a function that takes in an array of numbers as an argument. 
+  In the body of the function, write logic to determine if the array is in 
+  ascending order. The function should return true, 
+  if it is sorted in ascending order, false if it is not. 
+  Create a variable, `arrayIsAscending` and set it equal to your
+  function invoked. Use the sample array to test this function.
 */
 
 //CODE HERE
+
+function arrayIsAscending(arr){
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] < arr[i + 1])
+    }
+}
 
 
 ////////////////// PROBLEM 22 ////////////////////
